46bb94a2663266125bd53dbb7f8fd29914c90cdd
frank <2434934228@qq.com>
2026-06-14 13:18:35 +0800

    feat(agent): add AgentProgressEvent type and pubsub progress broker

--- internal/pubsub/events.go
+++ internal/pubsub/events.go
@@ -24,6 +24,7 @@ const (
 	PayloadTypeSession                PayloadType = "session"
 	PayloadTypeFile                   PayloadType = "file"
 	PayloadTypeAgentEvent             PayloadType = "agent_event"
+	PayloadTypeAgentProgress          PayloadType = "agent_progress"
 	PayloadTypeConfigChanged          PayloadType = "config_changed"
 	PayloadTypeSkillsEvent            PayloadType = "skills_event"
 )
